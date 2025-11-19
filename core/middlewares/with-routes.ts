3a8d366ec6b02b94106a6f4bbe7d76157ec8e064
bc-andreadao <andrea.dao@bigcommerce.com>
2025-11-19 12:52:36 -0800

    add rewrites for other page and entity types

--- core/middlewares/with-routes.ts
+++ core/middlewares/with-routes.ts
@@ -41,22 +41,96 @@ const getRoute = async (path: string, channelId?: string) => {
   return response.data.site.route;
 };
 
+const getRawWebPageContentQuery = graphql(`
+  query getRawWebPageContent($id: ID!) {
+    node(id: $id) {
+      __typename
+      ... on RawHtmlPage {
+        htmlBody
+      }
+    }
+  }
+`);
+
+const getRawWebPageContent = async (id: string) => {
+  const response = await client.fetch({
+    document: getRawWebPageContentQuery,
+    variables: { id },
+  });
+
+  const node = response.data.node;
+
+  if (node?.__typename !== 'RawHtmlPage') {
+    throw new Error('Failed to fetch raw web page content');
+  }
+
+  return node;
+};
+
 export const withRoutes: MiddlewareFactory = () => {
   return async (request: NextRequest) => {
-
-    const route = await getRoute("/the-planter-by-rustic-roots", "1804873");
+    const route = await getRoute('/the-planter-by-rustic-roots', '1804873');
 
     const node = route?.node;
 
-    let url: string = `/en/product/${node?.entityId}`;
-    
+    let url: string;
+
+    switch (node?.__typename) {
+      case 'Brand': {
+        url = `/en/brand/${node.entityId}`;
+        break;
+      }
+
+      case 'Category': {
+        url = `/en/category/${node.entityId}`;
+        break;
+      }
+
+      case 'Product': {
+        url = `/en/product/${node.entityId}`;
+        break;
+      }
+
+      case 'NormalPage': {
+        url = `/en/webpages/${node.id}/normal/`;
+        break;
+      }
+
+      case 'ContactPage': {
+        url = `/en/webpages/${node.id}/contact/`;
+        break;
+      }
+
+      case 'RawHtmlPage': {
+        const { htmlBody } = await getRawWebPageContent(node.id);
+
+        return new NextResponse(htmlBody, {
+          headers: { 'content-type': 'text/html' },
+        });
+      }
+
+      case 'Blog': {
+        url = `/en/blog`;
+        break;
+      }
+
+      case 'BlogPost': {
+        url = `/en/blog/${node.entityId}`;
+        break;
+      }
+
+      default: {
+        const { pathname } = new URL(request.url);
+        url = `/en${pathname}`;
+      }
+    }
+
     const rewriteUrl = new URL(url, request.url);
 
-    console.log("NODE:", node);
-    console.log("ORIGINAL:", request.url);
-    console.log("TARGET:", url);
-    console.log("REWRITTEN:", rewriteUrl.toString());
+    console.log('NODE:', node);
+    console.log('ORIGINAL:', request.url);
+    console.log('TARGET:', url);
+    console.log('REWRITTEN:', rewriteUrl.toString());
     return NextResponse.rewrite(rewriteUrl);
   };
 };
-
